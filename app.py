05a1765fdaf65ecaa169d2ffda7786933f6c02f9
samirhimi <rhimi1111@gmail.com>
2024-04-13 23:10:37 +0000

    add ip address

--- app.py
+++ app.py
@@ -1,31 +1,24 @@
 from flask import Flask
-import socket 
+import socket
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def get_hostname():
+def get_pod_info():
     # Get the hostname of the pod
     hostname = socket.gethostname()
-    return f"Hostname: {hostname}"
-    # Get ip address
-    ip_address = socket.gethostbyname(hostname)
-    return f"IP Address: {ip_address}"
-    # Get pod name
-    pod_name = os.getenv("POD_NAME")
-    return f"Pod Name: {pod_name}"
-    # Get os version
-    os_version = os.getenv("OS_VERSION")
-    return f"OS Version: {os_version}"
-    # Get cpu info
-    cpu_info = os.getenv("CPU_INFO")
-    return f"CPU Info: {cpu_info}"
-    # Get mem info 
-    mem_info = os.getenv("MEM_INFO")
-    return f"Mem Info: {mem_info}"
-    # Get disk info
-    disk_info = os.getenv("DISK_INFO")
-    return f"Disk Info: {disk_info}"
+
+    # Get the IP address of the pod
+    pod_ip = os.environ.get('POD_IP')
+
+    return f"Hostname: {hostname}\nPod IP Address: {pod_ip}"
 
 if __name__ == '__main__':
+    # Get the pod IP address from environment variables
+    pod_ip = os.getenv('POD_IP', 'Unknown')
+    
+    # Print the pod IP address for reference
+    print(f"Pod IP Address: {pod_ip}")
+
     app.run(debug=True, host='0.0.0.0')
