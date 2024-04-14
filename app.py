122d22cb8ab1b08615194baf5260c32d089a005e
samirhimi <rhimi1111@gmail.com>
2024-04-14 10:27:32 +0000

    add some stuffs

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
 from flask import Flask
 import socket
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
+
 def get_pod_info():
     # Get the hostname of the pod
     hostname = socket.gethostname()
 
     # Get the IP address of the pod
-    pod_ip = os.environ.get('POD_IP')
+    pod_ip = socket.getaddrinfo(hostname)
 
     return f"Hostname: {hostname}\nPod IP Address: {pod_ip}"
 
 if __name__ == '__main__':
-    # Get the pod IP address from environment variables
-    pod_ip = os.getenv('POD_IP', 'Unknown')
-    
-    # Print the pod IP address for reference
-    print(f"Pod IP Address: {pod_ip}")
-
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000, debug=True)
