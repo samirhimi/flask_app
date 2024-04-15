8ea8a0806b795fb217098cd2bce849bdf109a8f1
samirhimi <rhimi1111@gmail.com>
2024-04-15 22:35:30 +0000

    refine the code again

--- static/scripts.js
+++ static/scripts.js
@@ -3,10 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(data => {
             document.getElementById('hostname').textContent = `Hostname: ${data.hostname}`;
-            document.getElementById('OS-version').textContent = `OS version: ${data.os_version}`;
-            document.getElementById('CPU-Number').textContent = `CPU Number: ${data.num_cpus}`;
             document.getElementById('cpu-usage').textContent = `CPU Usage: ${data.cpu_usage}%`;
-            document.getElementById('memory-usage').textContent = `Memory Usage: ${data.memory_usage} bytes`;
+            document.getElementById('memory-usage').textContent = `Memory Usage: ${data.mem_usage}%`;
             document.getElementById('ip-address').textContent = `IP Address: ${data.ip}`;
         })
         .catch(error => console.error('Error:', error));
